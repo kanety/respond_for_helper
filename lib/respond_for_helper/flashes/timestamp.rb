cd00149d432a0d7ac83b75f8bb30bfb973adee79
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2022-10-13 14:05:07 +0900

    Use Time.zone.now if possible

--- lib/respond_for_helper/flashes/timestamp.rb
+++ lib/respond_for_helper/flashes/timestamp.rb
@@ -5,7 +5,17 @@ module RespondForHelper
     class Timestamp < Base
       def initialize(type, options = {})
         super
-        @options = options.reverse_merge(timestamp: I18n.l(Time.now))
+        @options = options.reverse_merge(timestamp: I18n.l(now))
+      end
+
+      private
+
+      def now
+        if Time.respond_to?(:zone)
+          Time.zone.now
+        else
+          Time.now
+        end
       end
     end
   end
